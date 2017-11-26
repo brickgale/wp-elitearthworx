d870bfa6624813678bae7cddfb75bb333cf8f5ea
Brian Monsales <brianmonsales@gmail.com>
2017-11-27 07:27:12 +0800

    updates

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
-			</div>
-			<footer id="footer" role="contentinfo">
-				<div id="copyright">
-					<?php 
-						echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'customtheme' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); 
-					?>
-				</div>
-			</footer>
 		</div>
+		<footer id="footer" role="contentinfo">
+			<div id="copyright">
+				<?php 
+					echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'customtheme' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); 
+				?>
+			</div>
+		</footer>
 		<?php wp_footer(); ?>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -7,26 +7,24 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<div id="wrapper" class="hfeed">
+		<header id="header" role="banner">
+			<div class="header-con row bottom-xs bottom-sm">
+				<section id="branding" class="col-xs-12 col-sm-4">
+					<div id="site-title">
+						<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
+					</div>
+					<div id="site-description">
+						<?php bloginfo( 'description' ); ?>
+					</div>
+				</section>
 
-			<header id="header" role="banner">
-				<div class="header-con row bottom-xs bottom-sm">
-					<section id="branding" class="col-xs-12 col-sm-4">
-						<div id="site-title">
-							<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
-						</div>
-						<div id="site-description">
-							<?php bloginfo( 'description' ); ?>
-						</div>
-					</section>
+				<nav id="menu" role="navigation" class="col-xs-12 col-sm-8">
+					<!-- <div id="search">
+						<?php //get_search_form(); ?>
+					</div> -->
+					<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+				</nav>
+			</div>
+		</header>
 
-					<nav id="menu" role="navigation" class="col-xs-12 col-sm-8">
-						<!-- <div id="search">
-							<?php //get_search_form(); ?>
-						</div> -->
-						<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-					</nav>
-				</div>
-			</header>
-
-			<div id="container">
+		<div id="container">
