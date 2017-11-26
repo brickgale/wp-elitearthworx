d2620891b138ecc2a9c6ddce4076babb73afc165
Brian Monsales <brianmonsales@gmail.com>
2017-11-27 07:31:15 +0800

    added font

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 		<?php wp_head(); ?>
 	</head>
