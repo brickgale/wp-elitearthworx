e7ba72c474d8659d124e463131aafe4a6cf6bbf3
Brian Monsales <brianmonsales@gmail.com>
2018-02-24 02:06:52 +0800

    updates

--- page.php
+++ page.php
@@ -1,4 +1,17 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package elitearthworx
+ */
+get_header(); ?>
 	<section class="page-con" id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
